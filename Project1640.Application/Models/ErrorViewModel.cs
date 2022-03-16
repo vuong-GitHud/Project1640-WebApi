ab3bb1b9efbc03f866d0989cfefeea17c906c1fc
Duc truong <thduc.2000@gmail.com>
2022-03-16 19:02:35 +0700

    Create Project

---
+++ Project1640.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project1640.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
